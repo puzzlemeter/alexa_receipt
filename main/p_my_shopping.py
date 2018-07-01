b377feb50b7e630b129572a78b080b8f947d0814
LIANG Yung-Hsin <bingo605@gmail.com>
2018-07-01 20:07:45 +0900

    add logo and sample config

--- main/p_my_shopping.py
+++ main/p_my_shopping.py
@@ -4,9 +4,11 @@ import boto3
 from boto3.session import Session
 from boto3.dynamodb.conditions import Key, Attr
 from PIL import Image, ImageDraw, ImageFont
+import time
 import random
 
 config = {}
+MODE = "layout" # or "prod"
 
 def read_config():
     f = open("config.json", 'r')
@@ -26,15 +28,16 @@ def get_all(dynamodb):
     table    = dynamodb.Table('puzzle_meter')
     response = table.scan()
     records  = response["Items"]
+    print(records)
     return records
 
 def imgid_to_image(records):
     img_height = config["receipt_base_height"] + len(records) * config["text_height"]
-    img_width = config["receipt_base_width"]
+    img_width  = config["receipt_base_width"]
     img        = Image.new('RGB', (img_width, img_height), color = (255, 255, 255))
     draw       = ImageDraw.Draw(img)
     fontname   = '/Library/Fonts/Arial Unicode.ttf'
-    font       = ImageFont.truetype(fontname, 18)
+    font       = ImageFont.truetype(fontname, 30)
     imgid      = str(random.randrange(10**10,10**11))
 
     y = config["text_height"]
@@ -45,7 +48,7 @@ def imgid_to_image(records):
         item_price = str(price) + u"円"
 
         draw.text((10, y), item_name, fill = (0,0,0), font = font)
-        draw.text((130, y), item_price, fill = (0,0,0), font = font)
+        draw.text((config["text_right_axis"], y), item_price, fill = (0,0,0), font = font)
 
         y += config["text_height"]
 
@@ -55,19 +58,25 @@ def imgid_to_image(records):
 
 def printout(img_name):
     usb = printer.Serial("/dev/tty.P58A_F-DevB")
+    usb.image(config["logo_path"])
+    time.sleep(5.0)
     usb.image(img_name)
     usb.cut()
 #     usb.text("test\n")
-#     usb.text("test\n")
-#     usb.image(IMGSAD)
-#     usb.qr("You can readme from your smartphone")
-#     usb.barcode('1324354657687','EAN13',64,2,'','')
 
 def main():
     read_config()
-    dynamodb = access_to_dynamodb()
-    records  = get_all(dynamodb)
+    records = {}
+    if (MODE == "prod") :
+        dynamodb = access_to_dynamodb()
+        records  = get_all(dynamodb)
+    else :
+        print("modify layout")
+        f = open("response.json", 'r')
+        records = json.load(f)
+
     saved_name = imgid_to_image(records)
+    # print(saved_name)
     printout(saved_name)
 
 if __name__=='__main__':
